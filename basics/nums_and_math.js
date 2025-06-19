65e9f9a1b4f12e7096c82440bf7f2d2a67cc2eda
Tanya160204 <tanyasingh1602@gmail.com>
2025-06-20 01:16:57 +0530

    understanding Nums and keywords

---
+++ basics/nums_and_math.js
@@ -0,0 +1,21 @@
+const score=400
+console.log(score);
+
+const balance= new Number(100)
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+const otherNumber= 23.8964
+console.log(otherNumber.toPrecision(3));
+
+const hund=1000000
+console.log(hund.toLocaleString('en-IN'));
+
+//++++++++++++++ Maths ++++++++++++++++
+
+console.log(Math);
+
+
+
